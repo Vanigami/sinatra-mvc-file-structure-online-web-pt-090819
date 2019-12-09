0c7f5a014a52083d538d9a74cacb4bece6b993a1
Vanna Winland <vgwinland@gmail.com>
2019-12-09 14:43:02 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,13 +5,13 @@ attr_accessor :name, :breed, :age
 @@all = []
 
   def initialize
-    @name = name 
-    @breed = breed 
-    @age = age 
-    @@all << self 
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 end
